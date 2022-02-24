798d51575715e1148746d2cd9609447a7681fb42
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-02-25 00:15:34 +0300

    добавил пару комментов

--- section_3/3.6/conftest.py
+++ section_3/3.6/conftest.py
@@ -7,14 +7,15 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
+#передаем параметр из командной строки в переменную
     user_language = request.config.getoption('language')
 
     options = Options()
 
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+#подставляем язык
     browser = webdriver.Chrome(options=options)
     
     browser.implicitly_wait(3)
     yield browser
-    print("\nquit browser..")
     browser.quit()
