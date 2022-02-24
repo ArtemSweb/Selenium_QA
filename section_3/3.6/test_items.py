137ebc3aeeca03a2497bea9c9e8ff0b9f0e1b4fe
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-02-25 00:10:31 +0300

    Добавил решение на шаг 9 модуля 3.6

---
+++ section_3/3.6/test_items.py
@@ -0,0 +1,14 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_find_button_add_to_basket(browser):
+    """Проверяем, что на странице есть кнопка добавления в корзину"""
+#Запускаем браузер(само описание браузера в conftest.py)
+    browser.get(link)
+#можно поспать
+    time.sleep(3)
+#Найдем кнопку по селектору класс
+    btn_add = browser.find_element_by_css_selector(".btn-add-to-basket")
+#Проверим, что кнопка существует
+    assert btn_add, f'Кнопки {btn_add} не существует на странице'
